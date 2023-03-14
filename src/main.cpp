00bbf329e91783d38798d70de6d20cbcb923f3a9
jenan19 <jenan19@student.sdu.dk>
2023-03-14 12:22:03 +0100

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 #include <iostream>
-
+#include <opencv2/opencv.hpp>
 
 
 
@@ -8,6 +8,11 @@ int main() {
 
 
     std::cout << "henlo u stinky\n";
+
+    cv::Mat img = cv::imread("data/DinoSR/dinoSR0001.png");
+
+    cv::imshow("dino UwU", img);
+    cv::waitKey(0);
 
     return 1;
 }
