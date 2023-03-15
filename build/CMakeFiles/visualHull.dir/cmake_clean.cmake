dbc318e9951eb40c998958b8d766b2b9c3a93a6a
TKZome <60672398+TKZome@users.noreply.github.com>
2023-03-15 15:26:43 +0100

    voxel with Mr. Peter

---
+++ build/CMakeFiles/visualHull.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/visualHull.dir/src/main.cpp.o"
+  "visualHull"
+  "visualHull.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/visualHull.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <opencv2/opencv.hpp>
+#include <Eigen/Dense>
 
 void loadImages(std::string *path, std::string *seriesName,std::vector<cv::Mat> *images, int numberOfImages)
 {
@@ -33,9 +35,7 @@ void applyMorphology(std::vector<cv::Mat> *images, cv::Size maskSize_open,  cv::
 
     kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, maskSize_close);
     cv::morphologyEx(images->at(i), images->at(i), cv::MORPH_CLOSE,kernel);
-  }
-  
-    
+  } 
 }
 
 
@@ -45,10 +45,7 @@ void removeBackground(std::vector<cv::Mat> *images, double threshold, double max
     for (int i = 0; i < images->size(); i++)
     {
         cv::inRange(images->at(i),cv::Scalar(threshold, threshold, threshold), cv::Scalar(maxval, maxval, maxval),images->at(i));
-    }
-    
-
-
+    } 
 }
 
 void findContours(std::vector<cv::Mat> *images, std::vector<cv::Mat> *contours, double threshold)
@@ -100,6 +97,82 @@ void showImages(std::vector<cv::Mat> *images)
 }
 
 
+void read_parameters(std::string file_path,std::vector<std::vector<std::string>> &parameters)
+{ 
+    std::ifstream file(file_path); 
+
+    if(file.is_open())
+    { 
+        std::string text; 
+    
+        while(std::getline(file,text))
+        { 
+            std::string element;
+            std::stringstream p_line(text); 
+            parameters.push_back({});
+            while(std::getline(p_line, element, ' '))
+            {
+                parameters[parameters.size()-1].push_back(element);
+            }
+        }
+    } 
+    file.close(); 
+}
+
+void get_pmatrix(std::vector<std::vector<std::string>> parameters, std::vector<cv::Mat> &projections)
+{ 
+    cv::Mat K(3,3, cv::DataType<double>::type);
+    cv::Mat R(3,3, cv::DataType<double>::type);
+    cv::Mat t(3,1, cv::DataType<double>::type);
+    cv::Mat E(3,4, cv::DataType<double>::type); 
+    for(int i = 0; i < parameters.size(); i++)
+    { 
+        int count = 1;
+        for(int row = 0; row < 3; row++)
+        { 
+            for(int col = 0; col < 3; col++)
+            {
+                K.at<double>(row,col) = std::stod(parameters[i][count]); 
+                count++; 
+            }
+        } 
+        for(int row = 0; row < 3; row++) 
+        { 
+            for(int col = 0; col < 3; col++)
+            {
+                R.at<double>(row,col) = std::stod(parameters[i][count]); 
+                count++; 
+            }
+        }
+        for(int row = 0; row < 3; row++) 
+        { 
+            t.at<double>(row,0) = std::stod(parameters[i][count]); 
+            count++; 
+        }
+        cv::hconcat(R,t,E);
+        projections.push_back(K*E); 
+    }
+}
+
+void init_voxels(std::vector<double> xlim,std::vector<double> ylim, std::vector<double> zlim, std::vector<double> voxel_size)
+{
+    cv::Mat v(1,3, cv::DataType<double>::type);
+    v.at<double>(0) = std::abs(xlim[1]-xlim[0])/voxel_size[0];
+    v.at<double>(1) = std::abs(ylim[1]-ylim[0])/voxel_size[1];
+    v.at<double>(2) = std::abs(zlim[1]-zlim[0])/voxel_size[2];
+
+    cv::Mat v_act(1,3, cv::DataType<int>::type);
+    v_act.at<int>(0) = (int)v.at<double>(0)+1;
+    v_act.at<int>(1) = (int)v.at<double>(1)+1;
+    v_act.at<int>(2) = (int)v.at<double>(2)+1;
+    int total_number = v_act.at<int>(0)*v_act.at<int>(1)*v_act.at<int>(2); 
+
+    cv::Mat voxels(total_number, 4, cv::DataType<double>::type); 
+
+
+}
+
+
 int main() {
 
     std::cout << "henlo u stinky\n";
@@ -112,8 +185,13 @@ int main() {
     std::string path = "../data/DinoSR/";
     std::string series = "dinoSR";
     std::string output = "bin_images";
-    int numberOfimages = 16;
+    int numberOfimages = 16; 
+    std::vector<cv::Mat> projections;
+    std::vector<std::vector<std::string>> parameters;
 
+    read_parameters(path + "dinoSR_par.txt", parameters);
+    get_pmatrix(parameters, projections); 
+    init_voxels({0.0,1.0},{0.0,1.0},{0.0,1.0}, {20,20,20});
 
     loadImages(&path, &series, &images, numberOfimages);
 
@@ -121,11 +199,11 @@ int main() {
 
 
 
-    removeBackground(&images,30, 256);
+    //removeBackground(&images,30, 256);
     
     //showImages(&images);
 
-    applyMorphology(&images, cv::Size (5,5), cv::Size (13,13));
+    //applyMorphology(&images, cv::Size (5,5), cv::Size (13,13));
 
     //showImages(&images);
 
@@ -135,7 +213,7 @@ int main() {
 
     //cv::imshow("dino UwU", img);
 
-    writeImages(&images, &path, &output);
+    //writeImages(&images, &path, &output);
 
     //cv::waitKey(1);
 
