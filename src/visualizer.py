011b208325143aaa6cd3925c7f27c76a0dc896f4
jenan19 <jenan19@student.sdu.dk>
2023-03-28 13:38:03 +0200

    vizulizer better but broken :(

--- src/visualizer.py
+++ src/visualizer.py
@@ -2,15 +2,50 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import genfromtxt
+import mcubes
+
+voxel3D = genfromtxt('data/voxelFile.csv', delimiter=',')
 
 
-my_data = genfromtxt('data/voxelFile.csv', delimiter=',')
+
+error_amount = 5
+maxv = np.max(voxel3D[1:,3])
+
+iso_value = maxv-np.round(((maxv)/100)*error_amount)-0.5
+print('max number of votes:' + str(maxv))
+print('threshold for marching cube:' + str(iso_value))
+print('voxel shape: ', voxel3D.shape)
+
+points_x = np.unique(np.array(voxel3D[1:, 0]))
+points_y = np.unique(np.array(voxel3D[1:, 1]))
+points_z = np.unique(np.array(voxel3D[1:, 2]))
+
+
+voxel3D_score = np.zeros((points_x.shape[0], points_y.shape[0] - 1, points_z.shape[0] - 1))
+
+data_idx = 0
+for x in range(voxel3D_score.shape[0]):
+    for y in range(voxel3D_score.shape[1]):
+        for z in range(voxel3D_score.shape[2]):
+            voxel3D_score[x,y,z] = voxel3D[data_idx, 3]
+            data_idx +=1
 
 
 
-x = np.array(my_data[1:, 0])
-y = np.array(my_data[1:, 1]) 
-z = np.array(my_data[1:, 2]) 
+
+# Extract the 0-isosurface
+vertices, triangles = mcubes.marching_cubes(voxel3D_score, iso_value)
+
+# Export the result to sphere.dae
+mcubes.export_mesh(vertices, triangles, "DinoR_21.dae", "DinoR_21_")
+
+
+
+
+
+"""x = np.array(voxel3D[1:, 0])
+y = np.array(voxel3D[1:, 1]) 
+z = np.array(voxel3D[1:, 2]) 
 
 
 
@@ -25,4 +60,4 @@ ma = np.random.choice([0,1], size=(10,10,10), p=[0.99, 0.01])
 #print(ma)
 ax.scatter3D(x,y,z,c=colors, cmap = 'Greens')
 
-plt.show()
+plt.show()"""
